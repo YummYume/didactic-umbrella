57a906190200e2bb08fac20042959ba93684ab0b
LeoBruant <bruantleo@gmail.com>
2024-06-25 10:37:02 +0200

    Feat : Theme
    Signed-off-by: LeoBruant <bruantleo@gmail.com>

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,24 +1,90 @@
-import aspectRatioPlugin from '@tailwindcss/aspect-ratio';
-import containerQueriesPlugin from '@tailwindcss/container-queries';
-import formsPlugin from '@tailwindcss/forms';
-import typographyPlugin from '@tailwindcss/typography';
-import plugin from 'tailwindcss/plugin';
+import { fontFamily } from "tailwindcss/defaultTheme";
+import type { Config } from "tailwindcss";
 
-import type { Config } from 'tailwindcss';
+const config: Config = {
+	darkMode: ["class"],
+	content: ["./src/**/*.{html,js,svelte,ts}"],
+	safelist: ["dark"],
+	theme: {
+		container: {
+			center: true,
+			padding: "2rem",
+			screens: {
+				"2xl": "1400px"
+			}
+		},
+		extend: {
+			colors: {
+				// primary: {
+				// 	'50': '#fff4ed',
+				// 	'100': '#ffe7d5',
+				// 	'200': '#ffcaa9',
+				// 	'300': '#fea573',
+				// 	'400': '#fd743a',
+				// 	'500': '#fb4f14',
+				// 	'600': '#ec340a',
+				// 	'700': '#c4230a',
+				// 	'800': '#9b1e11',
+				// 	'900': '#7d1c11',
+				// 	'950': '#440a06',
+				// },
+				// secondary: {
+				// 	'50': '#f1fcfb',
+				// 	'100': '#d0f7f5',
+				// 	'200': '#a1eeeb',
+				// 	'300': '#6adede',
+				// 	'400': '#35b3b7',
+				// 	'500': '#22a4aa',
+				// 	'600': '#188189',
+				// 	'700': '#18666d',
+				// 	'800': '#175358',
+				// 	'900': '#184449',
+				// 	'950': '#08262b',
+				// },
+				border: "hsl(var(--border) / <alpha-value>)",
+				input: "hsl(var(--input) / <alpha-value>)",
+				ring: "hsl(var(--ring) / <alpha-value>)",
+				background: "hsl(var(--background) / <alpha-value>)",
+				foreground: "hsl(var(--foreground) / <alpha-value>)",
+				primary: {
+					DEFAULT: "hsl(var(--primary) / <alpha-value>)",
+					foreground: "hsl(var(--primary-foreground) / <alpha-value>)"
+				},
+				secondary: {
+					DEFAULT: "hsl(var(--secondary) / <alpha-value>)",
+					foreground: "hsl(var(--secondary-foreground) / <alpha-value>)"
+				},
+				destructive: {
+					DEFAULT: "hsl(var(--destructive) / <alpha-value>)",
+					foreground: "hsl(var(--destructive-foreground) / <alpha-value>)"
+				},
+				muted: {
+					DEFAULT: "hsl(var(--muted) / <alpha-value>)",
+					foreground: "hsl(var(--muted-foreground) / <alpha-value>)"
+				},
+				accent: {
+					DEFAULT: "hsl(var(--accent) / <alpha-value>)",
+					foreground: "hsl(var(--accent-foreground) / <alpha-value>)"
+				},
+				popover: {
+					DEFAULT: "hsl(var(--popover) / <alpha-value>)",
+					foreground: "hsl(var(--popover-foreground) / <alpha-value>)"
+				},
+				card: {
+					DEFAULT: "hsl(var(--card) / <alpha-value>)",
+					foreground: "hsl(var(--card-foreground) / <alpha-value>)"
+				}
+			},
+			borderRadius: {
+				lg: "var(--radius)",
+				md: "calc(var(--radius) - 2px)",
+				sm: "calc(var(--radius) - 4px)"
+			},
+			fontFamily: {
+				sans: [...fontFamily.sans]
+			}
+		}
+	},
+};
 
-export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  darkMode: 'class',
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    typographyPlugin,
-    formsPlugin,
-    aspectRatioPlugin,
-    containerQueriesPlugin,
-    plugin(({ addVariant }) => {
-      addVariant('hocus', ['&:enabled:hover', '&:enabled:focus-visible']);
-    }),
-  ],
-} as Config;
+export default config;
